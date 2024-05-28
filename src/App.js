5730f1f496974ad434869b6582a5660ac1c120fe
tanish-goloakit <tanish@golokait.com>
2024-05-29 00:15:39 +0530

    chamges

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="https://www.coinpayments.net/index.php" method="post">
+	<input type="hidden" name="cmd" value="_pay_simple"/>
+	<input type="hidden" name="reset" value="1"/>
+	<input type="hidden" name="merchant" value="a42270bad6e33209eb5d554a4ec073ef"/>
+	<input type="hidden" name="item_name" value="NFT listing"/>
+	<input type="hidden" name="item_desc" value="NFT listing"/>
+	<input type="hidden" name="currency" value="USD"/>
+	<input type="hidden" name="amountf" value="2.00000000"/>
+	<input type="hidden" name="want_shipping" value="0"/>
+	<input type="image" src="https://www.coinpayments.net/images/pub/buynow-grey.png" alt="Buy Now with CoinPayments.net"/>
+</form>
+
     </div>
   );
 }
